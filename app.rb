34d920624670d59276e1b3e7c9f4897ebbe8c0e9
nhatminhdao <nhatminhdao@gmail.com>
2024-07-20 01:51:33 +0000

    edit

--- app.rb
+++ app.rb
@@ -1,9 +1,51 @@
 require "sinatra"
 require "sinatra/reloader"
+# so that we dont need to shut down the server and restart everytime we make a change
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @computer_move = moves.sample
+  
+  if @computer_move == "rock"
+    @outcome = "tied"
+  elsif @computer_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:rock)
+  
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  @computer_move = moves.sample
+  if @computer_move == "rock"
+    @outcome = "won"
+  elsif @computer_move == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+
+  erb(:paper)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @computer_move = moves.sample
+  if @computer_move == "rock"
+    @outcome = "lost"
+  elsif @computer_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+  
+  erb(:scissors)
 end
